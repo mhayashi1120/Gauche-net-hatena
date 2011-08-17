189ef9e9561af4efdf6a2a144980ad6146d81987
Masahiro Hayashi <mhayashi1120@gmail.com>
2011-08-18 01:22:31 +0900

    fix parsing web page

--- net/hatena/diary/web.scm
+++ net/hatena/diary/web.scm
@@ -1,17 +1,19 @@
 (define-module net.hatena.diary.web
   (use gauche.charconv)
   (use net.hatena.util)
+  (use parser.peg)
   (use rfc.822)
-  (use rfc.cookie)
   (use rfc.base64)
-  (use rfc.md5)
+  (use rfc.cookie)
   (use rfc.http)
   (use rfc.json)
+  (use rfc.md5)
   (use srfi-1)
   (use srfi-13)
+  (use srfi-14)
+  (use srfi-19)
   (use text.tr)
   (use util.list)
-  (use parser.peg)
 
   (export
    diary-log
@@ -23,8 +25,7 @@
    diary-entry
    diary-post
 
-   ;; TODO testing
-   diary-newest-log
+   diary-newest-log diary-today-log
   ))
 (select-module net.hatena.diary.web)
 
@@ -59,11 +60,10 @@
                   `((rkm ,(rkm-value cookie))
                     (mode "json")
                     (name ,name)
-                    (file :file ,file)
+                    (file ,(read-as-bytes file))
                     ;; TODO to send multiple file?
-                    ;; (num 1)
-                    ;; (max 1)
-                    ))))
+                    (num 0)
+                    (max 0)))))
 
     (define (retrieve status headers body)
       (check-status status headers body)
@@ -88,7 +88,8 @@
 
       (let1 alist (parse-json-string (code-convert body))
         ;;TODO about newline
-        (values (assoc-ref alist "title") (string-append (assoc-ref alist "body") "\n"))))
+        (values (assoc-ref alist "title")
+                (string-append (assoc-ref alist "body") "\n"))))
 
     (call-with-values call retrieve)))
 
@@ -111,28 +112,26 @@
            ((#/filename=(.+)/ (cadr pair)) =>
             (lambda (m) (m 1))))))
       (check-status status headers body)
-      ;;TODO return values
       (values body (parse-filename)))
 
     (call-with-values call retrieve)))
 
+;; return log list that is not yet contained `diary-log'
+;; TODO FQDN to IP address.
 (define (diary-newest-log cred)
   (with-web-session cred cookie
-    (define (call)
-      (let* ((cid 1)
-             (date "2011-04-08") ;;TODO
-             (type "daily")
-             (params (make-query-params cid date type)))
-        (web-get cookie hatena-cuonter-host
-                 #`"/,(ref cred 'username)/log?,(compose-query params)")))
+    (retrieve-log-page cred cookie 1)))
 
-    (define (retrieve status headers body)
-      (check-status status headers body)
-      (print body)
-      ;;TODO return values
-      (peg-parse-string %html-logs body))
-
-    (call-with-values call retrieve)))
+(define (diary-today-log cred)
+  (with-web-session cred cookie
+    (let loop ((res '())
+               (page 1))
+      (receive (log next)
+          (retrieve-log-page cred cookie page)
+        (if next
+          (loop (append res log) next)
+          (sort (delete-duplicates (append res log))
+                (^ (x y) (string>? (car x) (car y)))))))))
 
 ;;
 ;; Low level API
@@ -140,30 +139,37 @@
 
 (define (web-login cred)
   (define (call)
-    (let1 query (http-compose-query #f (list 
-    									(list :name (ref cred 'username)) 
-    									(list :password (ref cred 'password)))
-    								'utf-8)
-      (http-post "www.hatena.ne.jp" "/login" query :no-redirect #t)))
+    (let1 query (http-compose-query 
+                 "/login"
+                 `((:name ,(ref cred 'username)) 
+                   (:password ,(ref cred 'password)))
+                 'utf-8)
+      (http-post "www.hatena.ne.jp" query #f :no-redirect #t :secure #t)))
 
   (define (retrieve status headers body)
     (check-status status headers body)
-    ;; create cookie string
-    (string-join
-     (construct-cookie-string
-      (filter
-       (lambda (c)
-         (and (string? (car c))
-              (member (car c) '("b" "rk"))))
-       (concatenate
-        (map
+    (let1 cookies (concatenate
+                   (map
+                    (lambda (c)
+                      (parse-cookie-string (list-ref c 1)))
+                    (filter (lambda (x)
+                              (and (pair? x)
+                                   (string-ci=? (car x) "set-cookie")))
+                            headers)))
+      (unless (and (assoc "b" cookies)
+                   (assoc "rk" cookies))
+        (error <hatena-api-error>
+               :status status :headers headers :body body
+               "Login failed"))
+      ;; create cookie string
+      (string-join
+       (construct-cookie-string
+        (filter
          (lambda (c)
-           (parse-cookie-string (list-ref c 1)))
-         (filter (lambda (x)
-                   (and (pair? x)
-                        (string-ci=? (car x) "set-cookie")))
-                 headers)))))
-     "; "))
+           (and (string? (car c))
+                (member (car c) '("b" "rk"))))
+         cookies))
+       "; ")))
 
   (call-with-values call retrieve))
 
@@ -183,8 +189,9 @@
      (base64-encode-string
       (md5-digest-string string))
      0 22))
-  (let1 c (parse-cookie-string cookie)
-    (md5-base64 (cadr (assoc "rk" c)))))
+  (and-let* ((c (parse-cookie-string cookie))
+             (rk (assoc "rk" c)))
+    (md5-base64 (cadr rk))))
 
 (define (web-post cookie server request-uri body . args)
   (apply http-post server request-uri body 
@@ -230,16 +237,6 @@
   
   )
 
-(define %html-char 
-  (let ((%special-char ($or
-                        ($do [($string "&gt;")] ($return ">"))
-                        ($do [($string "&lt;")] ($return "<"))
-                        ($do [($string "&amp;")] ($return "&"))
-                        ($do [($string "&quot;")] ($return "\""))
-                        ($do [($string "&nbsp;")] ($return " "))))
-        (%raw-char ($do [c anychar] ($return c))))
-    ($or %special-char %raw-char)))
-
 (define-class <log-item> ()
   (
    (time :init-keyword :time)
@@ -251,67 +248,178 @@
    (referer :init-keyword :referer)
    ))
 
-(define %html-logs 
-  (let* ((%ws ($skip-many ($one-of #[ \t\r\n])))
-         (%begin-tag ($char #\<))
-         (%begin-endtag ($seq %begin-tag ($char #\/)))
-         (%end-tag ($char #\>))
-         ;;FIXME `>' in attribute value
-         (%rest-of-tag ($seq ($skip-many ($none-of #[>])) %end-tag)))
+(define (retrieve-log-page cred cookie page)
+  (define (call)
+    (let* ((cid 1)
+           (date (date->string (current-date) "~Y-~m-~d"))
+           (type "daily")
+           (params (make-query-params page cid date type)))
+      (web-get cookie hatena-cuonter-host
+               #`"/,(ref cred 'username)/log?,(compose-query params)")))
 
-    (define %begin-logtable-tag 
-      ($seq %begin-tag 
-            %ws ($string "table") 
-            %ws ($string "id=\"log_table\"") 
-            %ws ($char #\>)))
+  (define (retrieve status headers body)
+    (check-status status headers body)
+    (parse-logs body))
 
-    (define %table-row
-      (let* ((%ws ($skip-many ($one-of #[ \t\r\n])))
-             (%begin ($string "<td>"))
-             (%end ($string "</td>")))
+  (call-with-values call retrieve))
 
-        (define ($getter %getter)
-          ($do %ws %begin
-               %ws [val %getter]
-               %ws %end
-               %ws
-               ($return (rope->string val))))
+(define %html-char 
+  (let ((%special-char 
+         ($or
+          ($do [($s "&gt;")] ($return #\>))
+          ($do [($s "&lt;")] ($return #\<))
+          ($do [($s "&amp;")] ($return #\&))
+          ($do [($s "&quot;")] ($return #\"))
+          ($do [($s "&nbsp;")] ($return #\space))))
+        (%raw-char ($do [c ($none-of #[<>])] ($return c))))
+    ($or %special-char %raw-char)))
 
-        (let ((%rawcell ($getter ($many-till %html-char %end)))
-              (%hrefcell ($getter ($or
-                                   ($do
-                                    [($many-till %ws ($string "<a"))]
-                                    [($string "<a href='")]
-                                    [url ($many-till %html-char ($char #\'))]
-                                    [($many-till anychar %end)]
-                                    ($return url))
-                                   ($do
-                                    [($many-till anychar %end)]
-                                    ($return ""))))))
-          ($do
-           %ws [($string "<tr>")]
-           [time %rawcell]
-           [url %hrefcell]
-           [agent %rawcell]
-           [lang %rawcell]
-           [display %rawcell]
-           [host %rawcell]
-           [referer %hrefcell]
-           %ws [($string "</tr>")]
-           %ws
-           ($return (make <log-item> :time time :url url :user-agent agent 
-                          :lang lang :display display 
-                          :host host :referer referer))))))
-    
-    (let* ((%before-table ($many-till anychar %begin-logtable-tag)))
+(define (parse-logs html)
+  (define %ws
+    ($skip-many ($one-of #[ \t\r\n])))
 
-      ($do
-       %before-table
-       %begin-logtable-tag
-       %ws
-       [($between ($string "<tr>") ($many-till anychar ($string "</tr>")) ($string "</tr>"))]
-       [texts ($many %table-row)]
-       [$return texts]))))
+  (define %html-attr
+    ($do 
+     [v ($many ($one-of #[a-zA-Z0-9]))]
+     ($return (apply string v))))
+
+  (define ($html-attr-value quote)
+    ($do 
+     %ws
+     [($c quote)]
+     [v ($many
+         ($none-of (list->char-set (list quote))))]
+     [($c quote)]
+     ($return (apply string v))))
+
+  (define %html-value
+    ($or
+     ($html-attr-value #\")
+     ($html-attr-value #\')
+     ($do [v ($many ($none-of #[ \t\n>]) 1)] 
+          ($return (apply string v)))))
+
+  (define %html-attribute
+    ($do
+     [name %html-attr]
+     [($c #\=)]
+     [value %html-value]
+     %ws
+     ($return (list name value))))
+
+  (define %html-attributes
+    ($do
+     %ws
+     [attrs ($sep-by %html-attribute %ws)]
+     ($return attrs)))
+
+  (define ($html-start-tag tag)
+    ($try
+     ($do
+      [($c #\<)]
+      %ws
+      [($string-ci tag)]
+      [attrs %html-attributes]
+      [($c #\>)]
+      ($return attrs))))
+
+  (define ($html-end-tag tag)
+    ($try
+     ($seq
+      ($s "</")
+      ($string-ci tag)
+      ($c #\>))))
+
+  (define ($cell p)
+    ($do
+     %ws
+     [($html-start-tag "td")]
+     [v p]
+     [($html-end-tag "td")]
+     %ws
+     ($return (string-trim v))))
+
+  (define %table-item
+    (let ((%raw ($do 
+                 [v ($many %html-char)]
+                 ($return (apply string v))))
+          (%url 
+           ($or
+            ($do
+             %ws
+             [attrs ($html-start-tag "a")]
+             ;; throw away rest of results
+             [($many-till anychar ($html-end-tag "td"))]
+             ($return (car (assoc-ref attrs "href"))))
+            ($do
+             [v ($many-till %html-char ($html-end-tag "td"))]
+             ($return (apply string v))))))
+      ($try
+       ($do
+        %ws
+        [($html-start-tag "tr")]
+        [time ($cell %raw)]
+        [url ($cell %url)]
+        [agent ($cell %raw)]
+        [lang ($cell %raw)]
+        [display ($cell %raw)]
+        [host ($cell %raw)]
+        [referer ($cell %url)]
+        [($html-end-tag "tr")]
+        ($return 
+         (receive (display-size display-bit)
+             (cond
+              ((string= display "")
+               (values "" ""))
+              ((#/^(.*)x([0-9]+)$/ display) =>
+               (^m (values (m 1) (m 2))))
+              (else
+               (values display "")))
+           (list time host referer agent lang display-size display-bit url)))))))
+
+  (define %skip-table-header
+    ($seq
+     %ws
+     ($html-start-tag "tr")
+     ($many-till anychar ($html-end-tag "tr"))
+     ($html-end-tag "tr")
+     %ws))
+
+  (define %log-table-parser
+    ($do
+     ($many-till anychar 
+                 ($try 
+                  ($do [attrs ($html-start-tag "table")] 
+                       (if (and-let* ((p (assoc-ref attrs "id"))
+                                      (id (car p))
+                                      (pred (equal? id "log_table")))
+                             #t)
+                         ($return #t)
+                         ($fail "Not yet")))))
+     [($html-start-tag "table")]
+     %skip-table-header
+     [c ($many-till anychar ($html-end-tag "table"))]
+     [d ($html-end-tag "table")]
+     ($return (apply string c))))
+
+  (define %log-row-parser
+    ($many %table-item))
+
+
+  (let* ((p (open-input-string html))
+         (table (peg-parse-port %log-table-parser p))
+         (log (peg-parse-string %log-row-parser table)))
+    (port-seek p 0)
+    (let1 next
+        (let loop ((l (read-line p)))
+          (cond
+           ((eof-object? l) #f)
+           ;; magic word
+           ((#/<a[ \t]+href=".*page=([0-9]+).*>次の[0-9]+件/ l) =>
+            (^m (string->number (m 1))))
+           (else
+            (loop (read-line p)))))
+      (values log next))))
 
 ;;TODO multiple textarea?
 (define %html-textarea 
@@ -358,18 +466,6 @@
 (define (web-diary-entries cred date)
   (diary-entry cred date))
 
-(define %html-attribute
-  (let* ((%html-attrkey anychar)
-         (%ws ($skip-many ($one-of #[ \t\r\n])))
-         (%dquote ($char #\"))
-         (%squote ($char #\'))
-         (%html-attrval ($or
-                         ($between %dquote ($not %dquote) %dquote)
-                         ($between %squote ($not %squote) %squote)
-                         ($many-till ($or ($not %ws) ($not ($char #\>)))
-                                     ($or %ws ($char #\>))))))
-    ($seq %html-attrkey ($char #\=) %html-attrval)))
-
 ;;TODO list
 ;; http://d.hatena.ne.jp/mhayashi1120/archive?mode=edit&mid=1
 ;; twitter_notification_enabled=0
@@ -391,6 +487,18 @@
       (code-convert body))
 
     (call-with-values call retrieve)))
+
+;; TODO file->string some byte (ex: 0xc2) destroy the rest of bytes 
+(define (read-as-bytes file)
+  (use gauche.uvector)
+  (let1 f (open-input-file file)
+    (let loop ((c (read-byte f))
+               (res '()))
+      (cond
+       ((eof-object? c)
+        (u8vector->string (list->u8vector (reverse res))))
+       (else
+        (loop (read-byte f) (cons c res)))))))
 
 (define (hatena-now)
   (* (sys-time) 1000))
